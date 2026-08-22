9029dabdad8c3b571305886aa676ddbade166a1b
AungThu-Phyo <6731503046@lamduan.mfu.ac.th>
2026-08-22 22:40:26 +0700

    feat: add API health endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
-});
+// app.get("/message", (c) => {
+//   return c.text("Hello Hono!");
+// });
+
+app.get("/api/health", (c) => {
+  return c.json({
+    status: "ok",
+    service: "advising-platform-api",
+  });
+})
 
 export default app;
